d2592220787dc666b775f10d76908f87de4ac920
naiwen <sunivengg@gmail.com>
2024-01-05 10:54:12 +0800

    refactor: change the title bar style to overlay

--- src/Root.vue
+++ src/Root.vue
@@ -1,5 +1,6 @@
 <template>
-  <div className="font-sans h-full w-full bg-pink-100 p-8">
+  <div class="font-sans h-full w-full bg-pink-100 p-8">
+    <div class="fixed w-full top-0 left-0 h-7 cursor-pointer" data-tauri-drag-region></div>
     <router-view></router-view>
   </div>
 </template>
